62139878a4ff9afa987cb61fd50a7c0454188c2e
khushigor2410 <60733578+khushigor2410@users.noreply.github.com>
2020-02-06 15:14:53 +0530

    THANK YOU

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+var fs= require("fs");
+var cors=require("cors");
+app.use(express.static("ROOT"));
+app.use(cors());
+let data = fs.readFileSync("./posts.json");
+let json=JSON.parse(data)
+console.log(json);
+
+
+json.push({"title":"Rohan","desc":"Nayak"});
+fs.writeFileSync("./posts.json",
+	JSON.stringify(json,null,2)
+	)
+app.use(express.json());
+
+//get method
+app.get("/post",(req,res)=>{
+	// json.push({"title":"Rohan","desc":"Nayak"})
+	res.send(json);
+
+// res.send(req.params)
+//res.send(req.query);
+})
+
+//post method api
+app.post("/post",(req,res)=>{
+	json.push(req.body);
+	fs.writeFileSync("./posts.json",JSON.stringify(json,null,2));
+	res.send(json);
+})
+
+
+
+
+
+
+app.listen(3000,()=>{console.log("listening on port 3000")});
